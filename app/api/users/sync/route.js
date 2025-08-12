e21d7137ed6053261f5ca8e8f97ca1b404c3fc37
gana3011 <gana301104@gmail.com>
2025-08-12 13:33:17 +0530

    changed next_base_url to app_base_url

--- app/api/users/sync/route.js
+++ app/api/users/sync/route.js
@@ -14,7 +14,7 @@ export async function POST(req) {
     
     // Convert the gql object to string
     const query = print(SYNC_USER);
-    const response = await axios.post(`${process.env.NEXT_BASE_URL}/api/graphql`, {
+    const response = await axios.post(`${process.env.APP_BASE_URL}/api/graphql`, {
       query,
       variables: { userId, name, email, picture, roles, created_at }
     });
@@ -25,57 +25,6 @@ export async function POST(req) {
       return new Response('GraphQL Error', { status: 500 });
     }
 
-  //   const idCookie = serialize("id", id.toString(), {
-  //   httpOnly: true,
-  //   secure: false,
-  //   sameSite: "lax",
-  //   path: "/",
-  //   maxAge: 60 * 60 * 24 * 7,
-  // });
-  
-  // const rolesCookie = serialize("roles", JSON.stringify(roles), {
-  //   httpOnly: true,
-  //   secure: false,
-  //   sameSite: "lax",
-  //   path: "/",
-  //   maxAge: 60 * 60 * 24 * 7,
-  // });
-
-  // return new Response("Done", {
-  //   status: 200,
-  //   headers: {
-  //     "Set-Cookie": `${idCookie}; ${rolesCookie}`,
-  //     "Content-Type": "text/plain",
-  //   },
-  // });
-
-    // const nextResponse = NextResponse.json({message:"Done"});
-
-    // try {
-    //   // const cookieStore = await cookies();
-      
-    //   // Set cookies
-    //   nextResponse.cookies.set("id", id.toString(), {
-    //     httpOnly: true,
-    //     secure: false,
-    //     sameSite: "Lax",
-    //     path: "/",
-    //     maxAge: 60 * 60 * 24 * 7 // 1 week
-    //   });
-      
-    //   // Store roles (stringified since it's likely an array)
-    //   nextResponse.cookies.set("roles", JSON.stringify(roles), {
-    //     httpOnly: true,
-    //     secure: false,
-    //     sameSite: "Lax",
-    //     path: "/",
-    //     maxAge: 60 * 60 * 24 * 7
-    //   });
-      
-    // } catch (err) {
-    //   console.error("Error setting cookies:", err);
-    //   return new Response("Failed to set cookies", { status: 500 });
-    // }
     return new Response('Done', { status: 200 });
   }catch (err) {
     console.error('Error: ' + err);
